bc9e86cd5f4cd84dd9c76bbea4020c1625d1b6c1
Ewelina Nowak <ewel.nowak@gmail.com>
2022-06-06 10:23:18 +0200

    Try and try and try

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const path = require('path');
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 
 const testimonialsRoutes = require('./routes/testimonials.routes.js');
 const concertsRoutes = require('./routes/concerts.routes.js');
 const seatsRoutes = require('./routes/seats.routes.js');
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(cors());
 
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
 app.use(express.static(path.join(__dirname, '/client/build')));
+
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
